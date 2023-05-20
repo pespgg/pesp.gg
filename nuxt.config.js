272c61efc8db29f0c6b452c8b013eb8503b086ba
Yizack Rangel <yizackr@gmail.com>
2023-05-20 23:51:58 +0200

    chore: declare android chrome icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
         { rel: "preload", href: "/fonts/Gilroy-Bold.woff", as: "font", type: "font/woff" },
         { rel: "preload", href: "/fonts/Gilroy-Regular.woff", as: "font", type: "font/woff" },
         { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-chrome-192x192.png" },
         { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
         { rel: "manifest", href: "/site.webmanifest" },
