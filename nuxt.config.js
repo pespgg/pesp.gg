a0ee8fecefd90389a2fecfb0b5c6424658ea1d0e
Yizack Rangel <yizackr@gmail.com>
2023-05-20 14:22:43 +0200

    chore: favicons setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,14 +20,20 @@ export default {
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
         { name: "robots", content: "index, follow" },
-        { name: "apple-mobile-web-app-title", content: SITE.name.full },
-        { name: "application-name", content: SITE.name.full },
+        { name: "apple-mobile-web-app-title", content: SITE.name.web },
+        { name: "application-name", content: SITE.name.web },
         { name: "msapplication-TileColor", content: "#071018" },
-        { name: "theme-color", content: "#071018" }
+        { name: "theme-color", content: "#071018" },
+        { name: "apple-mobile-web-app-capable", content: SITE.name.web }
       ],
       link: [
         { rel: "preload", href: "/fonts/Gilroy-Bold.woff", as: "font", type: "font/woff" },
         { rel: "preload", href: "/fonts/Gilroy-Regular.woff", as: "font", type: "font/woff" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#f80357" },
         ...preload_banners
       ]
     }
