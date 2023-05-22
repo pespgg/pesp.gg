c248c2a910495ec72e76e99638c76bc6316f2b70
Yizack Rangel <yizackr@gmail.com>
2023-05-22 22:46:40 +0200

    chore: preload images middleware

--- app.vue
+++ app.vue
@@ -11,19 +11,3 @@ setScrollBehavior();
     </Transition>
   </NuxtLayout>
 </template>
-
-<script>
-export default {
-  mounted () {
-    // Preload banners
-    const banners = Object.keys(import.meta.glob("~/public/images/banners/*.jpg")).map((path) => {
-      return path.replace(/^.*[\\/]/, "");
-    });
-
-    banners.forEach((banner) => {
-      const img = new Image();
-      img.src = `/images/banners/${banner}`;
-    });
-  }
-};
-</script>
---
+++ middleware/preload.global.js
@@ -0,0 +1,14 @@
+export default defineNuxtRouteMiddleware(() => {
+  if (process.server) {
+    return;
+  }
+  const banners = Object.keys(import.meta.glob("~/public/images/banners/*.jpg")).map((path) => {
+    return path.replace(/^.*[\\/]/, "");
+  });
+
+  banners.forEach((banner) => {
+    const img = new Image();
+    img.src = `/images/banners/${banner}`;
+  });
+  console.log("preload");
+});
