292e4d1bb0b0ac3fb21a8c49b14cf93bff52f9b9
Yizack Rangel <yizackr@gmail.com>
2023-05-22 21:13:12 +0200

    style: temp fix text-dark

--- pages/servicios.vue
+++ pages/servicios.vue
@@ -2,12 +2,12 @@
   <!-- Banner -->
   <BannerPage banner="servicios.jpg" :text="t('servicios')" />
   <section id="servicios">
-    <div class="container py-5">
+    <div class="container my-5">
       <template v-for="(servicio, i) of SCHEMA.servicios" :key="i">
         <div :id="servicio.title" class="row px-2 px-sm-0 bg-dark rounded overflow-hidden" :class="{ 'mb-4': i < SCHEMA.servicios.length - 1 }">
           <!-- Info col -->
           <div class="col-md-8 col-lg-6 p-sm-4 shadow" :class="i % 2 ? 'order-1 order-md-2' : 'order-1 order-md-1'">
-            <h3 class="text-uppercse mb-4 mt-4"><b>{{ t(servicio.title) }}</b></h3>
+            <h3 class="text-uppercse mb-4 mt-4"><strong>{{ t(servicio.title) }}</strong></h3>
             <!-- eslint-disable-next-line vue/no-v-html -->
             <p v-for="(p, j) of servicio.content" :key="j" v-html="t(p)" />
           </div>
@@ -20,4 +20,14 @@
     </div>
   </section>
   <StatsBand :stats="SCHEMA.stats.experience" />
+  <section id="marcas">
+    <div class="bg-white my-5 py-5 text-dark">
+      <div class="text-center text-dark">
+        <h1>
+          <strong>Ellos confían en nosotros</strong>
+        </h1>
+        Marcas patrocinadoras de nuestros eventos
+      </div>
+    </div>
+  </section>
 </template>
