09afcd561e6db78c8205d15f4935cda366e39fd6
Yizack Rangel <yizackr@gmail.com>
2023-06-01 14:13:50 +0200

    chore: check config

--- server/api/mail.post.js
+++ server/api/mail.post.js
@@ -2,6 +2,7 @@ import Handlebars from "handlebars";
 
 export default defineEventHandler(async (event) => {
   const { token, template, form } = await readBody(event);
+  const config = useRuntimeConfig(event);
   const { email, name, subject, message } = form;
   if (!token) {
     throw createError({
@@ -11,15 +12,16 @@ export default defineEventHandler(async (event) => {
   }
 
   const verify = await verifyTurnstileToken(token);
-  console.log(verify)
+  console.log(config);
+  console.log(verify);
   if (!verify.success) {
-    return success;
+    return verify.success;
   }
 
   const hbs = Handlebars.compile(templates[template]);
   const html = hbs();
 
-  const config = useRuntimeConfig(event);
+  
   const mail = await sendMail(config, {
     to: email,
     subject,
