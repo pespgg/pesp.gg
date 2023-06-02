71af9b47376d24d2ceaa116e57682fccb6843706
Yizack Rangel <yizackr@gmail.com>
2023-06-03 01:19:45 +0200

    chore: no padding on small screens

--- pages/p/[permalink].vue
+++ pages/p/[permalink].vue
@@ -41,7 +41,7 @@ useSeoMeta({
       </h1>
       <hr>
       <!-- eslint-disable-next-line vue/no-v-html -->
-      <div class="p-3">
+      <div class="p-0 p-lg-3">
         <LoadPost :permalink="post.permalink" />
       </div>
     </article>
