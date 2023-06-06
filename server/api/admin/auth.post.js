ce9a03f8df462f32f4ab4bce3483caa6f552e014
Yizack Rangel <yizackr@gmail.com>
2023-06-06 23:53:26 +0200

    fix: login

--- server/api/admin/auth.post.js
+++ server/api/admin/auth.post.js
@@ -3,7 +3,7 @@ import { eq, and } from "drizzle-orm";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { user, pass } = body;
-  const login = useDb().select().from(tables.admins).where(and(eq(tables.admins.username, user), eq(tables.admins.password, pass))).get();
+  const login = await useDb().select().from(tables.admins).where(and(eq(tables.admins.username, user), eq(tables.admins.password, pass))).get();
   if (!login.username) {
     throw createError({
       statusCode: 401,
