03d7f6a384a2534408dcdf949e471e0a62f856ff
Yizack Rangel <yizackr@gmail.com>
2023-10-05 20:42:02 -0500

    refactor: email script

--- scripts/emailTemplates.ts
+++ scripts/emailTemplates.ts
@@ -2,7 +2,7 @@ import { readFileSync, writeFileSync, readdirSync } from "fs";
 import chalk from "chalk";
 
 const path = "./assets/email/templates";
-const templates = {};
+const templates: Record<string, string> = {};
 readdirSync(path).forEach((file) => {
   const name = file.replace(".html", "");
   templates[name] = readFileSync(`${path}/${file}`, "utf8").replace(/\r\n/g, "").replace(/\s+/g, " ");
