862eac16f607663b682d5f340b2804f6211f2b3f
Yizack Rangel <yizackr@gmail.com>
2023-10-05 19:49:45 -0500

    chore: refactor `plugins` (#19)

--- plugins/bootstrap.client.ts
+++ plugins/bootstrap.client.ts
@@ -2,26 +2,27 @@ import "bootstrap/js/dist/offcanvas";
 import "bootstrap/js/dist/dropdown";
 import Carousel from "bootstrap/js/dist/carousel";
 import Modal from "bootstrap/js/dist/modal";
+
 class Bootstrap {
-  hideModal (id) {
+  hideModal (id: string) {
     const instance = Modal.getInstance(id);
     if (instance) {
       instance.hide();
     }
   }
 
-  showModal (id) {
+  showModal (id: string) {
     const modal = new Modal(id);
     modal.show();
   }
 
-  startCarousel (id) {
+  startCarousel (id: string) {
     const instance = Carousel.getOrCreateInstance(id);
     if (instance) {
       instance.cycle();
     }
   }
-};
+}
 
 const bootstrap = new Bootstrap();
 
