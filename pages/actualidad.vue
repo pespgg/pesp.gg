2a74bd1dce62cdb661e03c25110ad89b73dd9cc1
Yizack Rangel <yizackr@gmail.com>
2023-10-07 08:17:08 -0500

    chore: add blogposting schemaorg

--- pages/actualidad.vue
+++ pages/actualidad.vue
@@ -35,13 +35,15 @@ useHead({
         <p class="m-0">{{ t("actualidad_info") }}</p>
       </div>
       <div class="row row-gap-3">
-        <div v-for="(post, i) of posts" :key="i" class="col-md-6 col-lg-4">
+        <div v-for="(post, i) of posts" :key="i" class="col-md-6 col-lg-4" itemscope itemtype="https://schema.org/BlogPosting">
           <article class="card mx-auto border-0 shadow h-100 light">
-            <img :src="getPostImage(post.permalink, post.updated)" class="card-img-top">
+            <img :src="getPostImage(post.permalink, post.updated)" class="card-img-top" itemprop="image">
             <div class="card-body bg-dark">
               <h4 class="card-title">
                 <strong>
-                  <NuxtLink :to="`/p/${post.permalink}/`">{{ post.titulo }}</NuxtLink>
+                  <NuxtLink :to="`/p/${post.permalink}/`">
+                    <span itemprop="headline">{{ post.titulo }}</span>
+                  </NuxtLink>
                 </strong>
               </h4>
               <div class="bg-body-tertiary text-light text-center mb-2 rounded small text-uppercase" role="button">{{ getTagName(post.tag) }}</div>
@@ -50,7 +52,7 @@ useHead({
             <div class="card-footer bg-dark p-0 overflow-hidden">
               <div class="d-flex align-items-center ps-3">
                 <Icon class="text-light" name="solar:calendar-linear" size="1.1rem" />
-                <small class="text-body-secondary ms-1">
+                <small class="text-body-secondary ms-1" itemprop="datePublished">
                   {{ formatDate(post.fecha) }}
                 </small>
                 <NuxtLink class="hover ms-auto bg-primary text-light py-1 px-3" :to="`/p/${post.permalink}/`">
