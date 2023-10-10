0ed83051f6c249cfd217749b6463de905387d968
Ahmed Rangel <ahmedrangel@outlook.com>
2023-10-10 18:04:49 -0500

    chore: image gallery export improvement

--- utils/schema/galeria.ts
+++ utils/schema/galeria.ts
@@ -1,13 +1,3 @@
-export default [
-  "img1.jpg",
-  "img2.jpg",
-  "img3.jpg",
-  "img4.jpg",
-  "img5.jpg",
-  "img6.jpg",
-  "img7.jpg",
-  "img8.jpg",
-  "img9.jpg",
-  "img10.jpg",
-  "img11.jpg"
-];
+export default Object.keys(import.meta.glob("~/public/images/galeria/*.jpg")).map((path) => {
+  return path.replace(/^.*[\\/]/, "");
+});
