1cea94b5c6214464da1d5b94a4f6f2c2cde62fab
Ahmed Rangel <ahmedrangel@outlook.com>
2023-10-15 04:28:42 -0500

    chore: string update

--- components/NewsletterForm.vue
+++ components/NewsletterForm.vue
@@ -6,7 +6,7 @@
       <div class="accordion">
         <div class="accordion-item border-0">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panels-collapse0" aria-expanded="true" aria-controls="panels-collapse0">
-            <h5 class="accordion-header">{{ t("Selecciona tus intereses") }}</h5>
+            <h5 class="accordion-header">{{ t("boletin_interests") }}</h5>
           </button>
           <div id="panels-collapse0" class="accordion-collapse collapse bg-light" aria-labelledby="0">
             <div class="accordion-body form-group">
