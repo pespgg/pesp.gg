79f4356438ff4eebc3661a5f5864e3f1f654f4b9
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-26 18:27:40 -0500

    chore: use composition api + adjust

--- components/BackToTop.vue
+++ components/BackToTop.vue
@@ -1,29 +1,27 @@
+<script setup lang="ts">
+const scrolled = ref(false);
+const maxScroll = 600;
+
+const topFunction = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+onMounted(() => {
+  window.onscroll = () => {
+    scrolled.value = (document.body.scrollTop > maxScroll || document.documentElement.scrollTop > maxScroll);
+  };
+});
+</script>
+
 <template>
   <Transition name="tab">
-    <button v-if="scrolled" class="top rounded position-fixed m-3 end-0 bottom-0 btn btn-light btn-lg shadow d-none d-lg-block" role="button" @click="topFunction()"><Icon name="solar:arrow-up-bold" size="1.5rem" /></button>
+    <button v-if="scrolled" id="back-to-top" class="top rounded position-fixed m-4 end-0 bottom-0 btn btn-light btn-lg shadow d-none d-lg-block align-items-center" role="button" @click="topFunction()">
+      <span class="p-2 d-flex align-items-center">
+        <Icon name="ph:arrow-up-fill" size="1.5rem" />
+      </span>
+    </button>
   </Transition>
 </template>
-
-<script lang="ts">
-export default {
-  data () {
-    return {
-      scrolled: false,
-      maxScroll: 600
-    };
-  },
-  mounted () {
-    window.onscroll = () => {
-      this.scrolled = (document.body.scrollTop > this.maxScroll || document.documentElement.scrollTop > this.maxScroll);
-    };
-  },
-  methods: {
-    topFunction () {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
-  }
-};
-</script>
 
 <style scoped>
 .top {
