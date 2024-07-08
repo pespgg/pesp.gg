e9429b7c509da288973064b25276b46ce712315b
Yizack Rangel <yizackr@gmail.com>
2024-07-08 05:34:13 -0500

    chore: improve error page types

--- app/error.vue
+++ app/error.vue
@@ -1,12 +1,16 @@
-<script setup>
-const { statusCode, statusMessage, message } = useError().value;
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: { type: Object as () => NuxtError, required: true }
+});
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div>
-      <h1>Error {{ statusCode }}</h1>
-      <h5>Lo sentimos, ha ocurrido un error. {{ message || statusMessage }}</h5>
+      <h1>Error {{ error.statusCode }}</h1>
+      <h5>Lo sentimos, ha ocurrido un error. {{ error.statusMessage || error.message }}</h5>
       <p>Volver: <NuxtLink :to="SCHEMA_pages.inicio.to" class="text-decoration-underline">pesp.gg</NuxtLink></p>
     </div>
   </div>
