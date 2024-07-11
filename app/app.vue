b846f94e463c5c9d29984ae39f104a4e387b9b4f
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-11 00:14:53 -0500

    fix: duplicated transitions css

--- app/app.vue
+++ app/app.vue
@@ -24,15 +24,3 @@ onMounted(() => {
   </NuxtLayout>
   <ToastsController />
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease-in-out;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
